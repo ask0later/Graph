9611e73bba6fff25f2a9a80ea9870d3578a06a43
Yaroslav Kuznetsov <asklater@bk.ru>
2025-03-06 14:05:44 +0300

    added test about Ctor

--- tests/unit/tests.cpp
+++ tests/unit/tests.cpp
@@ -2,14 +2,27 @@
 
 #include <gtest/gtest.h>
 
-TEST(GraphTest, GraphCtor) {
+TEST(GraphTest, GraphCtor1) {
     graph::Graph graph {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}};
-
+    graph.DepthFirstSearch();
+    graph.BreadthFirstSearch();
     std::vector<size_t> v1{0, 0, 0, 0, 1, 2, 1, 3, 2, 3, 2, 4, 3, 4};
     std::vector<size_t> v2{4, 5, 7, 11, 6, 8, 0, 9, 10, 12, 1, 13, 2, 3};
     std::vector<size_t> v3{6, 10, 12, 13, 0, 1, 4, 2, 5, 7, 8, 3, 9, 11};
     
-    ASSERT_EQ(graph.GetIndeces(), v1);
-    ASSERT_EQ(graph.GetNextRibs(), v2);
-    ASSERT_EQ(graph.GetPrevRibs(), v3);
+    ASSERT_EQ(graph.GetIndices(), v1);
+    ASSERT_EQ(graph.GetNextEdges(), v2);
+    ASSERT_EQ(graph.GetPrevEdges(), v3);
+}
+
+TEST(GraphTest, GraphCtor2) {
+    graph::Graph graph {{1, 2}, {1, 3}};
+
+    std::vector<size_t> v1{0, 0, 0, 1, 2, 1, 3};
+    std::vector<size_t> v2{3, 4, 6, 5, 1, 0, 2};
+    std::vector<size_t> v3{5, 4, 6, 0, 1, 3, 2};
+    
+    ASSERT_EQ(graph.GetIndices(), v1);
+    ASSERT_EQ(graph.GetNextEdges(), v2);
+    ASSERT_EQ(graph.GetPrevEdges(), v3);
 }
