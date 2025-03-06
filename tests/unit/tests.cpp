2e515f4f5eb4ca75009add39a4fbb50704a96e23
Yaroslav Kuznetsov <asklater@bk.ru>
2025-03-06 15:56:10 +0300

    Fixed test

--- tests/unit/tests.cpp
+++ tests/unit/tests.cpp
@@ -4,8 +4,7 @@
 
 TEST(GraphTest, GraphCtor1) {
     graph::Graph graph {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}};
-    graph.DepthFirstSearch();
-    graph.BreadthFirstSearch();
+
     std::vector<size_t> v1{0, 0, 0, 0, 1, 2, 1, 3, 2, 3, 2, 4, 3, 4};
     std::vector<size_t> v2{4, 5, 7, 11, 6, 8, 0, 9, 10, 12, 1, 13, 2, 3};
     std::vector<size_t> v3{6, 10, 12, 13, 0, 1, 4, 2, 5, 7, 8, 3, 9, 11};
@@ -17,10 +16,12 @@ TEST(GraphTest, GraphCtor1) {
 
 TEST(GraphTest, GraphCtor2) {
     graph::Graph graph {{1, 2}, {1, 3}};
+    graph.BreadthFirstSearch();
+    graph.DepthFirstSearch();
 
-    std::vector<size_t> v1{0, 0, 0, 1, 2, 1, 3};
-    std::vector<size_t> v2{3, 4, 6, 5, 1, 0, 2};
-    std::vector<size_t> v3{5, 4, 6, 0, 1, 3, 2};
+    std::vector<size_t> v1{0, 0, 0, 0, 1, 2, 1, 3};
+    std::vector<size_t> v2{4, 5, 7, 3, 6, 1, 0, 2};
+    std::vector<size_t> v3{6, 5, 7, 3, 0, 1, 4, 2};
     
     ASSERT_EQ(graph.GetIndices(), v1);
     ASSERT_EQ(graph.GetNextEdges(), v2);
