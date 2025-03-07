27531126e25f8c74671bdc924da9651941474c06
Yaroslav Kuznetsov <asklater@bk.ru>
2025-03-07 17:20:59 +0300

    writed main function for a task

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include "graph.hpp"
+#include "bipartite.hpp"
+#include <iostream>
 
 int main() try {
-    graph::Graph<int, int> graph1 {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}};
-    graph1.Print();
-    graph::Graph<int, int> graph2 {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}};
-    graph2.Print();
+    graph::Graph<int, int> graph;
+    std::cin >> graph;
+    
+    graph::BipartiteVisitor<int, int> visitor;
+    graph::BipartiteChecker<int, int> checker(visitor);
+    
+    if (!checker.isBipartite(graph, graph::CheckingPolicy::DFS))
+        throw std::runtime_error("Graph is not bipartite");
+    
+    std::string output;
+    for (auto&& elem : visitor) {
+        output += std::to_string(elem.first + 1);
+        output += (elem.second == 0) ? " b " : " r ";
+    }
+    std::cout << output << std::endl;
 } catch (std::exception &ex) {
     std::cout << ex.what() << std::endl;
 };
