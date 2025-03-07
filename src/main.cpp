d2a838e6c95c335c9d6ff88231b054d022d1a098
Yaroslav Kuznetsov <asklater@bk.ru>
2025-03-07 19:49:38 +0300

    refactored for test-checking

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,17 @@ int main() try {
     graph::BipartiteVisitor<int, int> visitor;
     graph::BipartiteChecker<int, int> checker(visitor);
     
-    if (!checker.isBipartite(graph, graph::CheckingPolicy::DFS))
-        throw std::runtime_error("Graph is not bipartite");
+    if (!checker.isBipartite(graph, graph::CheckingPolicy::DFS)) {
+        std::cout << "Graph is not bipartite" << std::endl;
+        return 0;
+    }
     
     std::string output;
     for (auto&& elem : checker.GetColors()) {
-        output += std::to_string(elem.first + 1);
-        output += (elem.second == 0) ? " b " : " r ";
+        output += std::to_string(elem.first + 1) + " ";
+        output += (elem.second == 0) ? "b " : "r ";
     }
+    output.erase(std::prev(output.end()));
     std::cout << output << std::endl;
 } catch (std::exception &ex) {
     std::cout << ex.what() << std::endl;
