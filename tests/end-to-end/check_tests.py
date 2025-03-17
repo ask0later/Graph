250b56d11182472377df6373c7fd4eceeabd14ec
Yaroslav Kuznetsov <asklater@bk.ru>
2025-03-17 16:17:15 +0300

    add test in cmake and added examples

--- tests/end-to-end/check_tests.py
+++ tests/end-to-end/check_tests.py
@@ -1,18 +1,20 @@
+import sys
 from subprocess import run, Popen, PIPE
 from sys import executable
 
+generator = sys.argv[1]
 num_test = 1
 is_ok = True
-for i in range(1, 12):
-    str_data =  "tests/end-to-end/" + str(i) + ".dat"
+for i in range(1, 13):
+    str_data =  "tests/" + str(i) + ".dat"
     file_in = open(str_data, "r")
-    str_ans = "tests/end-to-end/" + str(i) + ".ans"
+    str_ans = "tests/" + str(i) + ".ans"
 
     ans = ""
     for i in open(str_ans):
         ans += i.strip()
 	
-    result = run(["build/src/main"], capture_output = True, encoding='cp866', stdin=file_in)
+    result = run([generator], capture_output = True, encoding='cp866', stdin=file_in)
     print("Test: " + str(num_test).strip())
 
     res = result.stdout.strip()
