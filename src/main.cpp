3754383b8b847b016981a3cf1de239ce4b46dec7
Yaroslav Kuznetsov <asklater@bk.ru>
2025-04-08 06:49:45 +0300

    add odd cycle output

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@ int main() try {
     graph::Graph<int, int> graph;
     std::cin >> graph;
     
-    graph::BipartiteVisitor<int, int> visitor;
+    graph::BipartiteVisitor<int, int> visitor(graph.GetVerticesCount());
     graph::BipartiteChecker<int, int> checker(visitor);
     
     if (!checker.isBipartite(graph, graph::CheckingPolicy::DFS)) {
         std::cout << "Graph is not bipartite" << std::endl;
+        for (auto &&elem : visitor.GetOddCycle())
+            std::cout << elem << " "; std::cout << std::endl;
         return 0;
     }
     
@@ -27,5 +29,5 @@ int main() try {
     output.erase(std::prev(output.end()));
     std::cout << output << std::endl;
 } catch (std::exception &ex) {
-    std::cout << ex.what() << std::endl;
+    std::cerr << ex.what() << std::endl;
 };
