b4529b65ba0089a9f175e1e026e259bf8c4359c7
Yaroslav Kuznetsov <asklater@bk.ru>
2025-04-08 06:52:40 +0300

    upgrade script

--- tests/end-to-end/check_tests.py
+++ tests/end-to-end/check_tests.py
@@ -1,34 +1,40 @@
 import sys
-from subprocess import run, Popen, PIPE
-from sys import executable
+from subprocess import run, PIPE
 
 generator = sys.argv[1]
 num_test = 1
 is_ok = True
-for i in range(1, 13):
-    str_data =  "tests/" + str(i) + ".dat"
-    file_in = open(str_data, "r")
-    str_ans = "tests/" + str(i) + ".ans"
 
-    ans = ""
-    for i in open(str_ans):
-        ans += i.strip()
-	
-    result = run([generator], capture_output = True, encoding='cp866', stdin=file_in)
-    print("Test: " + str(num_test).strip())
+for i in range(1, 14):
+    test_file = f"tests/{i}.dat"
+    ans_file = f"tests/{i}.ans"
+    
+    with open(test_file, "r") as f_in:
+        input_data = f_in.read()
+    
+    with open(ans_file, "r") as f_ans:
+        ans = f_ans.read().replace('\r\n', '\n').rstrip()
 
-    res = result.stdout.strip()
+    result = run([generator], input=input_data, capture_output=True, text=True, encoding='cp866')
+    res = (result.stdout + result.stderr).replace('\r\n', '\n').rstrip()
 
-    is_ok &= (res == ans)
-    if res == ans:
+    print(f"Test {num_test}:")
+    
+    is_current_ok = (res == ans)
+    is_ok &= is_current_ok
+    
+    if is_current_ok:
         print("OK")
     else:
-        print("ERROR\nExpect:", ans, "\nGive:", res)
+        print("ERROR")
+        print("Expected:\n", repr(ans))
+        print("Got:\n", repr(res))
+    
     print("-------------------------------------------------")
     num_test += 1
 
 if is_ok:
-	print("TESTS PASSED")
+    print("TESTS PASSED")
 else:
     print("TESTS FAILED")
     sys.exit(1)
